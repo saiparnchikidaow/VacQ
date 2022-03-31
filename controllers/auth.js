070b9901f79f9df422903a40c885922a5b777f01
saiparnchikidaow <saiparnnnhuhu@gmail.com>
2022-03-31 23:03:47 +0700

    Add Security

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,44 +65,6 @@ exports.login = async (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-
-    //Validate email and password
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Please enter email and password" });
-    }
-
-    //Check for user
-    const user = await User.findOne({ email }).select("+password");
-
-    if (!user) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Invalid credentials" });
-    }
-
-    //Check if password matches
-    const isMatch = await user.matchPassword(password);
-
-    if (!isMatch) {
-      return res
-        .status(400)
-        .json({ success: false, error: "Invalid credentials" });
-    }
-
-    sendTokenResponse(user, 200, res);
-  } catch (err) {
-    res.status(401).json({
-      success: false,
-      error: "Cannot convert email or password to string",
-    });
-  }
-};
-
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   //Create token
--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+
+//Route files
+const hospitals = require("./routes/hospitals");
+const auth = require("./routes/auth");
+const appointments = require("./routes/appointments");
+
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -9,19 +21,39 @@ dotenv.config({ path: "./config/config.env" });
 //connect DB here
 connectDB();
 
-//Route files
-const hospitals = require("./routes/hospitals");
-const appointments = require("./routes/appointments");
 const app = express();
-const auth = require("./routes/auth");
 
 //Body parser
 app.use(express.json());
+
+//Sanitize data
+app.use(mongoSanitize());
+
+//Set security headers
+app.use(helmet());
+
+//Prevent XSS attacks
+app.use(xss());
+
+//Rate Limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10mins
+  max: 100,
+});
+app.use(limiter);
+
+//Prevent http param pollutions
+app.use(hpp());
+
+//Enable CORS
+app.use(cors());
+
+//Cookie parser
+app.use(cookieParser());
+
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/appointments", appointments);
-//Cookie parser
-app.use(cookieParser());
 
 const PORT = process.env.PORT || 3000;
 
